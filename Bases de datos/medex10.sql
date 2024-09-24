5ff932625d0f843c92e49e1e4cd73ea6ec5d2370
cundiss <facucarignani@gmail.com>
2024-09-24 16:14:28 -0300

    Base de datos

---
+++ Bases de datos/medex10.sql
@@ -0,0 +1,285 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 24-09-2024 a las 21:13:20
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `medex`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `atendidos`
+--
+
+CREATE TABLE `atendidos` (
+  `atendido_id` int(11) NOT NULL,
+  `pacientes_id` int(11) NOT NULL,
+  `medico_id` int(11) NOT NULL,
+  `fecha_atencion` datetime NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `dni` varchar(10) NOT NULL,
+  `motivo` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `historial`
+--
+
+CREATE TABLE `historial` (
+  `historial_id` int(11) NOT NULL,
+  `paciente_id` int(11) NOT NULL,
+  `medico_id` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `detalle` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `medicos`
+--
+
+CREATE TABLE `medicos` (
+  `medico_id` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `rol` enum('medico','secretario') DEFAULT 'medico'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `medicos`
+--
+
+INSERT INTO `medicos` (`medico_id`, `nombre`, `email`, `password`, `rol`) VALUES
+(1, 'Franco', 'franco@gmail.com', '$2y$10$Z.xcgFsPrn3kkrs8aXrEq.JGhjhcAGP4Y3aboJKVeCX2pv.Z4IhIi', 'secretario'),
+(2, 'Gilberto', 'ElGilberXD@gmail.com', '$2y$10$YT9v3GLPThDdBqPKZdeCK.dLS.uNSJIpa8nw4saYjmxwvIaraWGq6', 'medico'),
+(3, 'Pepinillo', 'dasdas@gmail.com', '$2y$10$mxaAOUDgZwu9ziqNfZgp4uxMk8DdERLybf4ew5tVXlFlD8H0C6Ifq', 'medico');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pacientes`
+--
+
+CREATE TABLE `pacientes` (
+  `paciente_id` int(11) NOT NULL,
+  `medico_id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `edad` int(3) NOT NULL,
+  `dni` varchar(10) NOT NULL,
+  `mutual` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `telefono` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `pacientes`
+--
+
+INSERT INTO `pacientes` (`paciente_id`, `medico_id`, `nombre`, `apellido`, `edad`, `dni`, `mutual`, `email`, `telefono`) VALUES
+(64, 2, 'Maria', 'Moffa', 23, '14124214', 'IOMA', 'maria@gmail.com', '1421412'),
+(65, 2, 'Franco', 'Locaso', 23, '14124214', 'IOM', 'ElGilberXD@gmail.com', '1421412412'),
+(66, 2, 'Facundo', 'carignani', 43, '14124214', 'IOMA', 'ElGilberXD@gmail.com', '1421412412');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pacientes_eliminados`
+--
+
+CREATE TABLE `pacientes_eliminados` (
+  `paciente_eliminado_id` int(11) NOT NULL,
+  `paciente_id` int(11) NOT NULL,
+  `historial_id` int(11) NOT NULL,
+  `medico_id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `edad` int(3) NOT NULL,
+  `dni` varchar(10) NOT NULL,
+  `mutual` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telefono` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pacientes_eliminados_historial`
+--
+
+CREATE TABLE `pacientes_eliminados_historial` (
+  `historial_id` int(11) NOT NULL,
+  `paciente_id` int(11) NOT NULL,
+  `medico_id` int(11) NOT NULL,
+  `descripcion` text NOT NULL,
+  `fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pacientes_medicos`
+--
+
+CREATE TABLE `pacientes_medicos` (
+  `paciente_id` int(11) NOT NULL,
+  `medico_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `turnos`
+--
+
+CREATE TABLE `turnos` (
+  `turno_id` int(11) NOT NULL,
+  `paciente_id` int(11) NOT NULL,
+  `medico_id` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `horario` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `turnos`
+--
+
+INSERT INTO `turnos` (`turno_id`, `paciente_id`, `medico_id`, `fecha`, `horario`) VALUES
+(35, 64, 0, '2024-09-24', '08:00:00');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `atendidos`
+--
+ALTER TABLE `atendidos`
+  ADD PRIMARY KEY (`atendido_id`);
+
+--
+-- Indices de la tabla `historial`
+--
+ALTER TABLE `historial`
+  ADD PRIMARY KEY (`historial_id`);
+
+--
+-- Indices de la tabla `medicos`
+--
+ALTER TABLE `medicos`
+  ADD PRIMARY KEY (`medico_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indices de la tabla `pacientes`
+--
+ALTER TABLE `pacientes`
+  ADD PRIMARY KEY (`paciente_id`);
+
+--
+-- Indices de la tabla `pacientes_eliminados`
+--
+ALTER TABLE `pacientes_eliminados`
+  ADD PRIMARY KEY (`paciente_eliminado_id`);
+
+--
+-- Indices de la tabla `pacientes_eliminados_historial`
+--
+ALTER TABLE `pacientes_eliminados_historial`
+  ADD PRIMARY KEY (`historial_id`);
+
+--
+-- Indices de la tabla `pacientes_medicos`
+--
+ALTER TABLE `pacientes_medicos`
+  ADD PRIMARY KEY (`paciente_id`,`medico_id`),
+  ADD KEY `medico_id` (`medico_id`);
+
+--
+-- Indices de la tabla `turnos`
+--
+ALTER TABLE `turnos`
+  ADD PRIMARY KEY (`turno_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `atendidos`
+--
+ALTER TABLE `atendidos`
+  MODIFY `atendido_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `historial`
+--
+ALTER TABLE `historial`
+  MODIFY `historial_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT de la tabla `medicos`
+--
+ALTER TABLE `medicos`
+  MODIFY `medico_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `pacientes`
+--
+ALTER TABLE `pacientes`
+  MODIFY `paciente_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
+
+--
+-- AUTO_INCREMENT de la tabla `pacientes_eliminados`
+--
+ALTER TABLE `pacientes_eliminados`
+  MODIFY `paciente_eliminado_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT de la tabla `pacientes_eliminados_historial`
+--
+ALTER TABLE `pacientes_eliminados_historial`
+  MODIFY `historial_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `turnos`
+--
+ALTER TABLE `turnos`
+  MODIFY `turno_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `pacientes_medicos`
+--
+ALTER TABLE `pacientes_medicos`
+  ADD CONSTRAINT `pacientes_medicos_ibfk_1` FOREIGN KEY (`paciente_id`) REFERENCES `pacientes` (`paciente_id`),
+  ADD CONSTRAINT `pacientes_medicos_ibfk_2` FOREIGN KEY (`medico_id`) REFERENCES `medicos` (`medico_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
